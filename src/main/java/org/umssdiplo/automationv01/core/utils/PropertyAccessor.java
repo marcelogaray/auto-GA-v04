30e526bcfbb7e0880f46d6d8f84a2835f391e023
ivanMise <ivanMise@gmail.com>
2018-06-15 18:00:47 -0400

    Terea cucumber test LOGIN

--- src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyAccessor {
+    private static final String BROWSER = "browser";
+    private static final String BASE_URL = "baseurl";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+
     private static PropertyAccessor PropertyAccessor;
     private Properties properties;
 
@@ -26,5 +31,37 @@ public class PropertyAccessor {
             PropertyAccessor = new PropertyAccessor();
         }
         return PropertyAccessor;
+    }
+
+    public int getImplicitTimeWait() {
+        return Integer.parseInt(getDataProperty("implicitTimeWait"));
+    }
+
+    private String getDataProperty(String nameProperty) {
+        String property = System.getProperty(nameProperty);
+        if (property == null) {
+            return properties.getProperty(nameProperty);
+        }
+        return property;
+    }
+
+    public int getExplicitTimeWait() {
+        return Integer.parseInt(getDataProperty("explicitTimeWait"));
+    }
+
+    public String getBrowser() {
+        return getDataProperty(BROWSER);
+    }
+
+    public String getBaseUrl() {
+        return getDataProperty(BASE_URL);
+    }
+
+    public String getUser() {
+        return getDataProperty(USERNAME);
+    }
+
+    public String getPassword() {
+        return getDataProperty(PASSWORD);
     }
 }
