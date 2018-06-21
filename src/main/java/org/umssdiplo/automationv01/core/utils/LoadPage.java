0b0ad3cd30df3af5522a5ea1f2e397e4d503ac20
J David Piérola A <jdpapiero@gmail.com>
2018-06-20 21:45:13 -0400

    clean code

--- src/main/java/org/umssdiplo/automationv01/core/utils/LoadPage.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/LoadPage.java
@@ -2,7 +2,6 @@ package org.umssdiplo.automationv01.core.utils;
 
 import org.umssdiplo.automationv01.core.customwebdriver.ManageDriver;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
-import org.umssdiplo.automationv01.core.managepage.Menu.Menu;
 
 public final class LoadPage {
     public static Login loginPage() {
