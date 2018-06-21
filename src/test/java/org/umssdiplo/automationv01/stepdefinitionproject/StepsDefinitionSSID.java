95d1d7b52acae5313868567abc630d0dd4868dc1
marcelogaray <marcelo.garay@spigit.com>
2018-06-20 20:41:14 -0400

    First approach

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionSSID.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionSSID.java
@@ -4,11 +4,14 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.testng.Assert;
+import org.umssdiplo.automationv01.core.managepage.Home.Home;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class StepsDefinitionSSID {
     private Login login;
+    private Home home;
 
     @Given("^'SSI-D' pagina de inicio de sesion es cargada$")
     public void ssiDPaginaDeInicioDeSesionEsCargada() throws Throwable {
@@ -18,5 +21,23 @@ public class StepsDefinitionSSID {
     @And("^Ingresar los credenciales validos en la pagina 'Login' para ingresar al sistema$")
     public void ingresarLosCredencialesValidosEnLaPaginaLoginParaIngresarAlSistema() throws Throwable {
         login.setCredentials();
+    }
+
+    //SSID HOME
+    @And("^click menu Equipment en  pagina 'Home'$")
+    public void selectMenuEquipment() throws Throwable {
+        home.clickEquipment();
+    }
+
+    @And("^click subMenu Equipment en  pagina 'Home'$")
+    public void selectSubMenuEquipment() throws Throwable {
+        listEquipment = home.clickSubEquipment();
+    }
+
+    //SSID LIST EQUIPMENTS
+    @Then("^pagina 'Equipment List' cargada corectamente$")
+    public void listEquipmentPresent() throws Throwable {
+        boolean result = listEquipment.listEquipmetsPresent();
+        Assert.assertTrue(result);
     }
 }
