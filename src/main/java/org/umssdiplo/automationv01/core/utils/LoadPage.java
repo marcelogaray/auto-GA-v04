ba7b5c53d1ab8cfa0638a42dcd65e83c62159450
J David Piérola A <jdpapiero@gmail.com>
2018-06-20 20:06:49 -0400

    Add Menu page object

--- src/main/java/org/umssdiplo/automationv01/core/utils/LoadPage.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/LoadPage.java
@@ -2,11 +2,16 @@ package org.umssdiplo.automationv01.core.utils;
 
 import org.umssdiplo.automationv01.core.customwebdriver.ManageDriver;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
+import org.umssdiplo.automationv01.core.managepage.Menu.Menu;
 
 public final class LoadPage {
     public static Login loginPage() {
         ManageDriver.getInstance().getWebDriver()
                 .navigate().to(PropertyAccessor.getInstance().getBaseUrl());
         return new Login();
+    }
+
+    public static Menu menuPage() {
+        return new Menu();
     }
 }
--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionSSID.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionSSID.java
@@ -5,14 +5,21 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
+import org.umssdiplo.automationv01.core.managepage.Menu.Menu;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class StepsDefinitionSSID {
     private Login login;
+    private Menu menu;
+
+    private void loadPageObjects(){
+        login = LoadPage.loginPage();
+        menu = LoadPage.menuPage();
+    }
 
     @Given("^'SSI-D' pagina de inicio de sesion es cargada$")
     public void ssiDPaginaDeInicioDeSesionEsCargada() throws Throwable {
-        login = LoadPage.loginPage();
+        loadPageObjects();
     }
 
     @And("^Ingresar los credenciales validos en la pagina 'Login' para ingresar al sistema$")
