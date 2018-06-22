0af71689069804829af794a08164d8324679927c
J David Piérola A <jdpapiero@gmail.com>
2018-06-21 21:36:26 -0400

    RF02-TC-071: clean code

--- src/main/java/org/umssdiplo/automationv01/core/managepage/Position/Position.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/Position/Position.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.support.FindBy;
 import org.umssdiplo.automationv01.core.managepage.BasePage;
 import org.umssdiplo.automationv01.core.utils.CommonEvents;
 
-import java.util.List;
-
 public class Position extends BasePage {
     @FindBy(xpath = "//h1[contains(text(),'Cargos de la empresa')]")
     private WebElement title;
