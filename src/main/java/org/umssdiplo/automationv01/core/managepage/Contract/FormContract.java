3c2c08ef2e66e77e7ccd6eaa1f2758522e83d16b
borisytu2014 <borisytu2014@gmail.com>
2018-07-02 17:31:34 -0400

    actualizando

--- src/main/java/org/umssdiplo/automationv01/core/managepage/Contract/FormContract.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/Contract/FormContract.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.umssdiplo.automationv01.core.managepage.BasePage;
 import org.umssdiplo.automationv01.core.utils.CommonEvents;
-
 import java.util.List;
 import java.util.Map;
 
